629529a22627a634c491511f10165f1198008b61
Ubuntu <ubuntu@ip-172-31-25-246.ec2.internal>
2018-06-05 17:30:17 +0000

    Setting up distillery

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,8 @@ config :test, Test.Endpoint,
   http: [port: 4000],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
-  server: true,
-  code_reloader: false
+  #server: true,
+  #code_reloader: false
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -50,7 +50,7 @@ config :logger, level: :info
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start the server for all endpoints:
 #
-#     config :phoenix, :serve_endpoints, true
+     config :phoenix, :serve_endpoints, true
 #
 # Alternatively, you can configure exactly which server to
 # start per endpoint:
